ab8c2e5caafc95807ebf2ec409db8cf61938a235
Jonathan Canela <jonathancanela365@gmail.com>
2020-09-08 16:18:34 -0700

    Finished

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const postsRouter = require('./data/seeds/03-posts-router.js');
+
+const server = express();
+
+server.use(express.json());
+server.use('/api/posts', postsRouter);
+
+// endpoints
+
+
+module.exports = server;
